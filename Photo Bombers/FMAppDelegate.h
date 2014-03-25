3e6fbb2d3f4c6f4df446002dc59141d86694e591
Fred Myers <fred@fred-myers.com>
2014-03-25 09:12:39 -0400

    Initial Commit

---
+++ Photo Bombers/FMAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  FMAppDelegate.h
+//  Photo Bombers
+//
+//  Created by Fredrick Myers on 3/25/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
